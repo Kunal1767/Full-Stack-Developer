a241cbf33b97cd5f7e88a0db0b1ebed225e09e21
kunal <kunalvarshney512@gmail.com>
2024-08-13 12:38:37 +0530

    #

--- Day5/Session/script.js
+++ Day5/Session/script.js
@@ -36,9 +36,67 @@ let car =['maruti','apachi','tvs']
 // console.log(sliceB)
 
 bike.splice(1,1,'kl','lk')
-console.log(bike)
+// console.log(bike)
 // console.log(bike.indexOf('bajaj'))
 // console.log(bike.lastIndexOf('bmw'))
 
 
 // bike.forEach(bike=>console.log(bike))
+
+// let length = bike.map(bike=>bike.length)
+// console.log(length)
+
+// for(let i=0;i<bike.length;i++){
+//     console.log(bike[i])
+// }
+
+// for(let bik of bike){
+//     console.log(bik)
+// }
+
+// let vk = bike.filter(bike=>bike.length>3)
+// console.log(vk)
+
+// let jk= bike.reduce((sum,bik)=>sum+bik.length,0)
+// console.log(jk)
+
+let matrix=[
+    [1,2,3],[4,5,6],[7,8,9]
+]
+console.log(matrix[2][1])
+
+// spread operator
+let arr1=[1,2,3]
+let arr2=[...arr1,4,5,6]
+console.log(arr2)
+
+// rest operater
+function sum(...number){
+    return number.reduce((a,b)=>a+b)
+}
+console.log(sum(1,2,3,4,5,6))
+
+// array destructing
+let[first,Second,third] = bike
+console.log(first)
+console.log(Second)
+
+// array membership
+console.log(bike.includes('bmw'))
+
+
+// sorting arrays
+
+console.log(bike)
+const y=bike.sort()
+console.log(y)
+
+let num=[10,20,30,40]
+let max= Math.max(...num)
+let min= Math.min(...num)
+console.log(max)
+console.log(min)
+
+let arr=[1,7,4,5,2]
+console.log(arr.sort())
+
